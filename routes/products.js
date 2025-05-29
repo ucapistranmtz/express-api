cd0da2092129b32777cbccdc0700a0b78d72b3c7
Ulises <ucapistranmtz@gmail.com>
2025-05-29 16:19:09 -0500

    RESTful endpoints for products

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
 const router = express.Router();
-const products = [
+let products = [
   { id: 1, name: "Iphone se", price: 100 },
-  { id: 1, name: "Iphone 14", price: 300 },
+  { id: 2, name: "Iphone 14", price: 300 },
+  { id: 3, name: "Iphone 14", price: 300 },
 ];
 router.get("/", (req, res) => {
   res.status(200).json(products).send();
@@ -21,17 +22,35 @@ router.get("/:id", (req, res) => {
   res.status(200).json(product).send();
 });
 
-router.post('/',(req,res)=>{
-console.log(req.body);
-      const newProduct = {
-        id: products.length+1,
-        name:req.body.name,
-        price:req.body.price
-      };
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const maxId= Math.max  (... new Set(products.map(product=>product.id)));
+ 
+    console.log("Max Id", maxId);
+  const newProduct = {
+    id: maxId + 1,
+    name: req.body.name,
+    price: req.body.price,
+  };
 
-      products.push(newProduct);
+  products.push(newProduct);
 
-      res.status(201).json(newProduct).send();
-})
+  res.status(201).json(newProduct).send();
+});
+
+router.delete("/:id", (req, res) => {
+    console.log("Delete Request", req.params.id);
+  const productIndex = products.findIndex( (product)=> product.id === parseInt(req.params.id));
+  console.log("Product Index", productIndex);
+
+  if (productIndex === -1) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+  products = products.filter(
+    (product) => product.id !== parseInt(req.params.id)
+  );
+  console.log("Products", products);
+  return res.status(200).json({ message: "Product deleted successfully",remaingingProducts: products }).send();
+});
 
 module.exports = router;
