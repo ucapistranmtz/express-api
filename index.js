c7042979e9bc97a703174d6d9f825dff75e9d8bd
Ulises <ucapistranmtz@gmail.com>
2025-05-29 10:47:57 -0500

    Simple api test

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => { 
+
+    console.log(`Server is running at http://localhost:${port}`); 
+});
